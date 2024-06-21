0f0982536d5aace3b9d1e16a3029f6996cfe242b
Haydn-Maust <kyrothehero@comcast.net>
2024-06-21 17:00:03 -0500

    Added T-0 Teleport

---
+++ script/c133704.lua
@@ -0,0 +1,68 @@
+--T-0 Teleport
+local T0_SETNAME = 0x69ac
+local s,id=GetID()
+function s.initial_effect(c)
+    --Special Summon 1 Level 4 or lower "T-0" monster from your hand or Deck, 
+    --then send 1 card from your hand to the GY.
+    local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_HANDES)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.etele_target)
+	e1:SetOperation(s.etele_operation)
+	c:RegisterEffect(e1)
+
+    --If this card is sent to the GY by card effect:
+    --You can target 1 "T-0" card in your GY, except "T-0 Teleport"; add it to your hand.
+    local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(function(e) return e:GetHandler():IsReason(REASON_EFFECT) end)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+end
+
+function s.etele_filter(c,e,tp)
+	return c:IsSetCard(T0_SETNAME) and c:IsLevelBelow(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.etele_target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.etele_filter,tp,LOCATION_DECK+LOCATION_HAND,0,1,nil,e,tp) 
+        and Duel.IsExistingMatchingCard(Card.IsAbleToGrave,tp,LOCATION_HAND,0,1,c) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,0,LOCATION_DECK+LOCATION_HAND)
+    Duel.SetOperationInfo(0,CATEGORY_HANDES,0,0,tp,1)
+end
+function s.etele_operation(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.etele_filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
+	if #g<=0 or Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)<=0 then return end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local h=Duel.SelectMatchingCard(tp,Card.IsAbleToGrave,tp,LOCATION_HAND,0,1,1,nil)
+	if #h>0 then
+		Duel.BreakEffect()
+		Duel.SendtoGrave(h,REASON_EFFECT)
+	end
+end
+
+function s.thfilter(c)
+	return c:IsSetCard(T0_SETNAME) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.thfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.thfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+	end
+end
