a196d4e85edd9cba2de5a6cc990ea355ca6acef0
Haydn-Maust <kyrothehero@comcast.net>
2024-06-21 17:05:10 -0500

    bug fix

--- script/c133704.lua
+++ script/c133704.lua
@@ -51,7 +51,7 @@ function s.etele_operation(e,tp,eg,ep,ev,re,r,rp)
 end
 
 function s.thfilter(c)
-	return c:IsSetCard(T0_SETNAME) and c:IsAbleToHand()
+	return c:IsSetCard(T0_SETNAME) and c:IsAbleToHand() and not c:IsCode(id)
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.thfilter(chkc) end
