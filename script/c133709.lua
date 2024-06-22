e133d512845807644ea744e9ea900f037e7cdf7a
Haydn-Maust <kyrothehero@comcast.net>
2024-06-22 17:45:29 -0500

    Added T-0 Destroyer

---
+++ script/c133709.lua
@@ -0,0 +1,48 @@
+--T-0 Destroyer
+local T0_SETNAME = 0x69ac
+local s,id=GetID()
+function s.initial_effect(c)
+    --Once per turn (Quick Effect): You can target 1 card on the field: 
+    --Destroy it, then, if this card is still on the field, 
+    --you can draw cards equal to the number of cards your opponent controls, 
+    --then shuffle that number of cards -1 from your hand into the Deck.
+    local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DRAW+CATEGORY_TODECK)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
+	c:RegisterEffect(e1)
+end
+
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() end
+	if chk==0 then return Duel.IsExistingTarget(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+    local ct=Duel.GetFieldGroupCount(tp,0,LOCATION_ONFIELD)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(tp)
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,ct)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,0,tp,ct-1)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)>0 and e:GetHandler():IsOnField() then
+        local ct=Duel.GetFieldGroupCount(tp,0,LOCATION_ONFIELD)
+		Duel.BreakEffect()
+		if Duel.Draw(p,ct,REASON_EFFECT)==ct and ct-1 ~= 0 then
+			local g=Duel.GetMatchingGroup(Card.IsAbleToDeck,p,LOCATION_HAND,0,nil)
+			if #g==0 then return end
+            Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,p,HINTMSG_TODECK)
+			local sg=g:Select(p,ct,ct,nil)
+			Duel.SendtoDeck(sg,nil,2,REASON_EFFECT)
+		end
+	end
+end
