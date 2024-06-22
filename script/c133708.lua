1d4de72bc35e8c5316b0514604210b273c9c71b5
Haydn-Maust <kyrothehero@comcast.net>
2024-06-22 16:43:55 -0500

    Added T-0 Spinner

---
+++ script/c133708.lua
@@ -0,0 +1,87 @@
+--T-0 Spinner
+local T0_SETNAME = 0x69ac
+local s,id=GetID()
+function s.initial_effect(c)
+    --During your Main Phase, or if your opponent activates a card or effect: 
+    --You can target 1 "T-0" card you control; 
+    --Destroy it, also Special Summon this card from your hand.
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetRange(LOCATION_HAND)
+    e1:SetCountLimit(1,id)
+	e1:SetTarget(s.pop_tg)
+	e1:SetOperation(s.pop_op)
+	c:RegisterEffect(e1)
+
+    local e2=e1:Clone()
+	e2:SetType(EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_CHAIN_SOLVED)
+	e2:SetCondition(s.pop_con)
+	c:RegisterEffect(e2)
+
+    --During the Main Phase (Quick Effect): You can target 1 monster your opponent controls; 
+    --If that monster is still on the field when this effect resolves, shuffle it into the Deck, 
+    --otherwise return 1 card on the field to the hand. 
+    local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_TODECK)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
+	e3:SetCondition(s.spin_con)
+	e3:SetTarget(s.spin_tg)
+	e3:SetOperation(s.spin_op)
+	c:RegisterEffect(e3)
+end
+
+function s.pop_filter(c)
+	return c:IsFaceup() and (c:IsSetCard(T0_SETNAME))
+end
+function s.pop_tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsControler(tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.pop_filter,tp,LOCATION_ONFIELD,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local tc=Duel.SelectTarget(tp,s.pop_filter,tp,LOCATION_ONFIELD,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,tc,1,tp,0)
+end
+function s.pop_op(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT)>0 then
+		local c=e:GetHandler()
+	    if not c:IsRelateToEffect(e) then return end
+	    Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+
+function s.pop_con(e,tp,eg,ep,ev,re,r,rp)
+	return ep==1-tp
+end
+
+function s.spin_con(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()==PHASE_MAIN1 or Duel.GetCurrentPhase()==PHASE_MAIN2
+end
+function s.spin_filter(c)
+	return c:IsAbleToDeck()
+end
+function s.spin_tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and s.spin_filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.spin_filter,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectTarget(tp,s.spin_filter,tp,0,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
+end
+function s.spin_op(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)>0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectMatchingCard(tp,Card.IsAbleToHand,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+	end
+end
