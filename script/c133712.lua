418954606f6b37dfe4ddd9658664ff5a8a7ec8fc
Haydn-Maust <kyrothehero@comcast.net>
2024-08-01 14:48:54 -0500

    Noble Sangha of the Tenyi initial work

---
+++ script/c133712.lua
@@ -0,0 +1,83 @@
+--Noble Sangha of the Tenyi
+local TENYI_SETNAME = 0x12c
+local s,id=GetID()
+function s.initial_effect(c)
+	--Link Summon procedure: 2 monsters, including a "Tenyi" monster
+    c:EnableReviveLimit()
+	Link.AddProcedure(c,nil,2,2,s.link_check)
+
+	--This card is treated as a non-Effect Monster while face-up on the field.
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_REMOVE_TYPE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(TYPE_EFFECT)
+	e1:SetCondition(s.eqcon1)
+	c:RegisterEffect(e1)
+
+	--If this card is Special Summoned: You can add 1 "Tenyi" card from your Deck to your hand.
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCountLimit(1,id)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+
+	--You can banish this card from your GY, then target 5 of your banished "Tenyi" cards; shuffle them into the Deck.
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_TODECK)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetCost(aux.bfgcost)
+	e3:SetTarget(s.tdtg)
+	e3:SetOperation(s.tdop)
+	c:RegisterEffect(e3)
+
+end
+s.listed_series={TENYI_SETNAME}
+
+function s.link_check(g,lc,sumtype,tp)
+	return g:IsExists(Card.IsSetCard,1,nil,TENYI_SETNAME,lc,sumtype,tp)
+end
+
+function s.thfilter(c)
+	return c:IsSetCard(TENYI_SETNAME) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+
+function s.tdfilter(c)
+	return c:IsSetCard(TENYI_SETNAME) and c:IsAbleToDeck()
+end
+function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_REMOVED) and chkc:IsControler(tp) and s.tdfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.tdfilter,tp,LOCATION_REMOVED,0,5,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectTarget(tp,s.tdfilter,tp,LOCATION_REMOVED,0,5,5,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,5,0,0)
+end
+function s.tdop(e,tp,eg,ep,ev,re,r,rp)
+	local tg=Duel.GetTargetCards(e)
+	if #tg>0 then
+		Duel.SendtoDeck(tg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+	end
+end
